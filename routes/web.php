80c87416fd730d7a5d39e6019578c0e3598d4f21
mdiktushar <md.tushar1804@gmail.com>
2022-03-01 20:28:56 +0600

    2. Setting p views part 2

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
+Controller
+|--------------------------------------------------------------------------
+*/
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -13,10 +20,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
