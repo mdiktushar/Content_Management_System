f0f57fbe1a6aebbe56fa90bdab216ae4c9ac83cb
mdiktushar <md.tushar1804@gmail.com>
2022-03-11 20:50:48 +0600

    temp

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -1,5 +1,63 @@
 <x-admin-master>
     @section('content')
-        <h1>User Profile of {{$user->name}}</h1>
+        <div class="row">
+            <h1>User Profile of {{$user->name}}</h1>
+            <div class="col-sm-6">
+                <form action="" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div>
+                        <img src="" alt="" srcset="">
+                    </div>
+                    <div class="form-group">
+                        <input type="file" name="" id="">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input 
+                            type="text" 
+                            name="name"
+                            id="name" 
+                            class="form-control" 
+                            value="{{$user->name}}"
+                            required
+                        >
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input 
+                            type="text" 
+                            name="email"
+                            id="email" 
+                            class="form-control" 
+                            value="{{$user->email}}"
+                            required
+                        >
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input 
+                            type="password" 
+                            name="password"
+                            id="password" 
+                            class="form-control" 
+                        >
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input 
+                            type="password" 
+                            name="password_confirmation"
+                            id="password_confirmation" 
+                            class="form-control" 
+                        >
+                    </div>
+
+                    <button class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
     @endsection
 </x-admin-master>
